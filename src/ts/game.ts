a2f7837402c22e6c571c3147e9b6ef58ac27bbd6
Mussa Faragi <mussa.faragi@gmx.de>
2026-07-17 17:21:38 +0200

    overlay added

--- src/ts/game.ts
+++ src/ts/game.ts
@@ -16,3 +16,42 @@ function init(){
     }
 }
 
+
+
+const exitBtn = document.getElementById("exit-btn") as HTMLButtonElement | null;
+const popup = document.getElementById("popup") as HTMLElement | null;
+const overlay = document.getElementById("overlay") as HTMLElement | null;
+
+
+exitBtn?.addEventListener("click", () => {
+    popup?.classList.add("active");
+    overlay?.classList.add("overlay-add");
+});
+
+
+
+const backBtn = document.getElementById("back-btn");
+backBtn?.addEventListener("click", () => {
+    popup?.classList.remove("active");
+    overlay?.classList.remove("overlay-add");
+});
+
+
+const exitPopupBtn = document.getElementById("exit-popup") as HTMLButtonElement | null;
+exitPopupBtn?.addEventListener("click", () => {
+    window.location.href = "settings.html";
+});
+
+
+document.addEventListener("click", (e) => {
+    if (!popup?.classList.contains("active")) return;
+
+    const target = e.target as HTMLElement;
+
+    if (target.closest("#popup") || target.closest("#exit-btn")) {
+        return;
+    }
+
+    popup.classList.remove("active");
+    overlay?.classList.remove("overlay-add");
+});
